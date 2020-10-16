10bcdab65dfd9e77cd157a776420ef9364c2c5bf
Zana Flander <zana.flander@kiwi.com>
2020-10-16 17:46:05 +0200

    chore: install formik and orbit components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { TextLink } from "@kiwicom/orbit-components"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TextLink
+      href="https://formik.org/docs/tutorial"
+      external
+    >
+      Formik tutorial
+    </TextLink>
   );
 }
 
